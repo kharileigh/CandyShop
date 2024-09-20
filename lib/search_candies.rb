c5da41e88ac5227ded3b898ada999b72bc3e95a6
kharileigh <kharileighmiller@gmail.com>
2024-09-20 18:08:49 +0100

    search_candies -- Function Implemented / All Tests Pass

--- lib/search_candies.rb
+++ lib/search_candies.rb
@@ -1,19 +1,27 @@
-candies = [
-    { name: 'Aero', price: 1.99 },
-    { name: 'Skitties', price: 2.99 },
-    { name: 'Mars', price: 1.49 },
-    { name: 'Maltesers', price: 3.49 },
-    { name: 'Skittles', price: 1.49 },
-    { name: 'Starburst', price: 5.99 },
-    { name: 'Ricola', price: 1.99 },
-    { name: 'Polkagris', price: 5.99 },
-    { name: 'Pastila', price: 4.99 },
-    { name: 'Mentos', price: 8.99 },
-    { name: 'Raffaello', price: 7.99 },
-    { name: 'Gummi bears', price: 10.99 },
-    { name: 'Fraise Tagada', price: 5.99 }
-]
 
 def search_candies(prefix, max_price)
 
+    candies = [
+        { name: 'Aero', price: 1.99 },
+        { name: 'Skitties', price: 2.99 },
+        { name: 'Mars', price: 1.49 },
+        { name: 'Maltesers', price: 3.49 },
+        { name: 'Skittles', price: 1.49 },
+        { name: 'Starburst', price: 5.99 },
+        { name: 'Ricola', price: 1.99 },
+        { name: 'Polkagris', price: 5.99 },
+        { name: 'Pastila', price: 4.99 },
+        { name: 'Mentos', price: 8.99 },
+        { name: 'Raffaello', price: 7.99 },
+        { name: 'Gummi bears', price: 10.99 },
+        { name: 'Fraise Tagada', price: 5.99 }
+    ]
+
+    candies
+    # FILTER - based on prefix -- candy[:name].start_with(prefix)
+    # FILTER - below max price -- candy[:price] < max_price
+    # RETURN - array of matches -- candy[:name]
+        .select { |candy| candy[:name].start_with?(prefix) && candy[:price] < max_price }
+        .map { |candy| candy[:name] }
+    
 end
